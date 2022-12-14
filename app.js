2a1eaeef889deb3da6eaa614a21cee3ddbf69c0f
gayathri-kayapati <gayathrikayapati@gmail.com>
2022-12-14 14:55:56 +0530

    added error alert and fetching data from server

--- app.js
+++ app.js
@@ -4,8 +4,22 @@ var outputDiv = document.querySelector("#output");
 // outputDiv.innerText = "I am Gayathri";
 var serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
 
+function getTranslationUrl(text) {
+  return serverUrl + "?" + "text= " + text;
+}
+
+function errorHandler(error) {
+  console.log("error occured", error);
+  alert("Something wrong with server! try agin after some time.");
+}
+
 function clickEventHandler() {
-  outputDiv.innerText = txtInput.value;
+  // outputDiv.innerText = txtInput.value;
+  var inputTxt = txtInput.value;
+  fetch(getTranslationUrl(inputTxt))
+    .then((response) => response.json())
+    .then((json) => console.log(json.contents.translated))
+    .catch(errorHandler);
 }
 
 btnTranslate.addEventListener("click", clickEventHandler);
