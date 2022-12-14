10edb10c2cb1ce967ab1fc9fdf8d51eb274e49c1
gayathri-kayapati <gayathrikayapati@gmail.com>
2022-12-14 12:36:00 +0530

    server Url added

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 // outputDiv.innerText = "I am Gayathri";
+var serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
 
 function clickEventHandler() {
-  outputDiv.innerText = "asasdsasdsa" + txtInput.value;
+  outputDiv.innerText = txtInput.value;
 }
 
 btnTranslate.addEventListener("click", clickEventHandler);
